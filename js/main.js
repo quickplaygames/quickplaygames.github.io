53f6b2bcb7cc6725e788b2211e482f865a246dcd
LEGALISE PIRACY <74979271+Rahib777-7@users.noreply.github.com>
2023-08-05 09:51:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,13 +78,7 @@ $(window).on('load', function() {
 	    topSpacing: 60,
 	    bottomSpacing: 60
 	});
-	document.write( '<script>\n' );
-document.write( '// Create an audio element\n' );
-document.write( 'const audio = new Audio();\n' );
-document.write( '\n' );
-document.write( '// Set the source of the audio element\n' );
-document.write( 'audio.src = \"https://cdn.discordapp.com/attachments/1071560339788939424/1137217000922423366/e730a2a499307bab.mp3\";\n' );
-document.write( '\n' );
+
 
 
 })(jQuery);
