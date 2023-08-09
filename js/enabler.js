0e94b9f52fb3003d1d7671deab2bce8665c6aeca
LEGALISE PIRACY <74979271+Rahib777-7@users.noreply.github.com>
2023-08-09 13:19:52 -0400

    Update enabler.js

--- js/enabler.js
+++ js/enabler.js
@@ -1,7 +1,10 @@
-	document.write( '<script>\n' );
-document.write( '// Create an audio element\n' );
-document.write( 'const audio = new Audio();\n' );
-document.write( '\n' );
-document.write( '// Set the source of the audio element\n' );
-document.write( 'audio.src = \"https://cdn.discordapp.com/attachments/1071560339788939424/1137217000922423366/e730a2a499307bab.mp3\";\n' );
-document.write( '\n' );
+document.write('<script>\n');
+document.write('// Create an audio element\n');
+document.write('const audio = new Audio();\n');
+document.write('\n');
+document.write('// Set the source of the audio element\n');
+document.write('audio.src = "https://cdn.discordapp.com/attachments/1071560339788939424/1137217000922423366/e730a2a499307bab.mp3";\n');
+document.write('\n');
+document.write('// Append the audio element to the document body\n');
+document.write('document.body.appendChild(audio);\n');
+document.write('</script>');
